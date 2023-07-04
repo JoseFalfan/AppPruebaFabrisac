82d91ab740f5632472b181c7bac4c66caa4e31e8
Jose Falfan <jose.falfan1@gmail.com>
2023-07-03 23:10:47 -0400

    Primer Commit repaso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,80 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { DbService } from './services/db.service';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
   {
-    path: '',
+    path: 'register',
+    redirectTo: 'register',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'forgot-password',
+    redirectTo: 'forgot-password',
+    pathMatch: 'full'
+  },
+  {
+    path: 'profile',
+    redirectTo: 'profile',
+    pathMatch: 'full'
+  },
+  {
+    path: 'cutters',
+    redirectTo: 'cutters',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'e404',
+    pathMatch: 'full'
+  },
+
+
+
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/userLogin/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/userLogin/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./pages/userLogin/forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/user/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  {
+    path: 'cutters',
+    loadChildren: () => import('./pages/staff/cutters/cutters.module').then( m => m.CuttersPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule), 
+    canActivate: [DbService]
+  },
+  {
+    path: 'e404',
+    loadChildren: () => import('./pages/e404/e404.module').then( m => m.E404PageModule)
+  }
 ];
 
 @NgModule({
---
+++ src/app/pages/userLogin/login/login.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  ModelUser: string = '';
+  ModelPassword: string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  validation() {
+    console.log(this.ModelUser);
+    console.log(this.ModelPassword);
+  }
+
+
+}
